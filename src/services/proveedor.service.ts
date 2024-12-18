30bd6cb27d878b53dbe65a82399fdc3d9cfac55c
Jordy07 <chacchicarlosv@gmail.com>
2024-12-18 10:57:33 -0500

    Cambios finales 17/12/2024

--- src/services/proveedor.service.ts
+++ src/services/proveedor.service.ts
@@ -1,25 +1,25 @@
 import { AppDataSource } from "../config/db.config";
-import { EstadoAuditoria } from "../enums/estado-auditoria";
 import { Proveedor } from "../entities/proveedor";
+import { EstadoAuditoria } from "../enums/estado-auditoria";
 
 const repository = AppDataSource.getRepository(Proveedor);
 
 export const insertarProveedor = async (data: Partial<Proveedor>): Promise<Proveedor> => {
-    console.log('insertarProveedor::service',data)
     const newProveedor: Proveedor = await repository.save(data);
     return await repository.findOne({where: { idProveedor: newProveedor.idProveedor }});
 }
 
-export const listarProveedor = async(): Promise<Proveedor[]> => {
-    return await repository.find({where: {estadoAuditoria: EstadoAuditoria.ACTIVO}});
+export const listarProveedor = async (): Promise<Proveedor[]> => {
+    return await repository.find({where: { estadoAuditoria: EstadoAuditoria.ACTIVO }});
 }
 
 export const obtenerProveedor = async (idProveedor: number) => {
     return await repository.findOne({where: {estadoAuditoria: EstadoAuditoria.ACTIVO,idProveedor}})
 }
 
-export const actualizarProveedor = (idProveedor: number, data: any) => {
-    return {accion:`actualizarProveedor:${idProveedor}`};
+export const actualizarProveedor = async (idProveedor: number, data: Partial<Proveedor>) => {
+    await repository.update(idProveedor,data);
+    return obtenerProveedor (idProveedor);
 }
 
 export const darBajaProveedor = async (idProveedor: number) => {
